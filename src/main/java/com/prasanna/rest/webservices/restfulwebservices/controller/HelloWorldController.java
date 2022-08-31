4ae8f9a78c863b93b0251e698ba6c9a379ef00f7
prasannakumar2495 <prasannakumar2461995@gmail.com>
2022-08-31 15:20:35 +0530

    added i18n

--- src/main/java/com/prasanna/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/prasanna/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,14 +1,20 @@
 package com.prasanna.rest.webservices.restfulwebservices.controller;
 
-import com.prasanna.rest.webservices.restfulwebservices.dto.UserDao;
 import com.prasanna.rest.webservices.restfulwebservices.model.HelloWorldBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/getmethod")
     public String helloWorld() {
@@ -23,5 +29,13 @@ public class HelloWorldController {
     @GetMapping("/getmethodbeanpathvariable/{name}")
     public HelloWorldBean helloWorldBeanPath(@PathVariable String name) {
         return new HelloWorldBean("Hello World!" + name);
+    }
+
+    @GetMapping("/getmethod-international")
+    public String helloWorldInternational() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null,
+                "Default Message", locale);
+        //return "Hello World!";
     }
 }
